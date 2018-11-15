cbf0d273979cdb85b64ee773bc220e616601f4e6
kiddion <kiddion@localhost.localdomain001>
2018-11-14 21:53:13 -0500

    Update cloudgo

--- homework-cloudgo/service/server.go
+++ homework-cloudgo/service/server.go
@@ -5,11 +5,7 @@ import "github.com/kataras/iris"
 
 func NewApp() *iris.Application {
 	app := iris.Default()
-<<<<<<< HEAD
 
-=======
-	
->>>>>>> c4a7558cfc7294299d97797f2b4004549c754c26
 	//获取输入的两个参数并打印
 	app.Get("/{str1}/{str2}", func(ctx iris.Context) {
 		str1 := ctx.Params().Get("str1")
