7fa5f1c981167a7b00fa040216a40f2de45f4fc4
kiddion <kiddion@localhost.localdomain001>
2018-11-14 21:50:40 -0500

    Update cloudgo

--- homework-cloudgo/main.go
+++ homework-cloudgo/main.go
@@ -8,23 +8,28 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+//设置默认端口为8080
 const (
 	PORT string = "8080"
 )
 
 func main() {
+	//如果没有监听到端口，则设为默认端口
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = PORT
 	}
 
-	//
+	//允许用户可以通过 -p设置端口
 	pPort := flag.StringP("port", "p", PORT, "PORT for httpd listening")
 	flag.Parse()
 	if len(*pPort) != 0 {
 		port = *pPort
 	}
 
+	//新建服务
 	app := service.NewApp()
+
+	//服务启动
 	app.Run(iris.Addr(":" + port))
 }
--- homework-cloudgo/service/server.go
+++ homework-cloudgo/service/server.go
@@ -1,10 +1,12 @@
 package service
 
+//使用iris框架
 import "github.com/kataras/iris"
 
 func NewApp() *iris.Application {
 	app := iris.Default()
 
+	//获取输入的两个参数并打印
 	app.Get("/{str1}/{str2}", func(ctx iris.Context) {
 		str1 := ctx.Params().Get("str1")
 		str2 := ctx.Params().Get("str2")
